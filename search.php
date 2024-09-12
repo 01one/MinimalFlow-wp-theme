f09a73459e5715ab9b66697402fe02003a1c823c
01one <84787013+01one@users.noreply.github.com>
2024-09-12 12:55:22 +0600

    Update search.php

--- search.php
+++ search.php
@@ -6,7 +6,7 @@
 
         <header class="ui header">
             <h1 class="ui header">
-                <?php printf( esc_html__( 'Search Results for: %s', 'programmerbee.com' ), '<span>' . get_search_query() . '</span>' ); ?>
+                <?php printf( esc_html__( 'Search Results for: %s', 'minimalflow' ), '<span>' . get_search_query() . '</span>' ); ?>
             </h1>
         </header>
 
@@ -15,7 +15,7 @@
                 <div class="item">
                     <div class="content">
                         <header class="header">
-                            <h2 class="ui header"><a href="<?php the_permalink(); ?>" class="ui header" ><?php the_title(); ?></a></h2>
+                            <h2 class="ui header"><a href="<?php the_permalink(); ?>" class="ui header"><?php the_title(); ?></a></h2>
                         </header>
                         <div class="description">
                             <?php the_excerpt(); ?>
@@ -28,10 +28,10 @@
     <?php else : ?>
 
         <header class="ui header">
-            <h1 class="ui header"><?php esc_html_e( 'Nothing Found', 'programmerbee.com' ); ?></h1>
+            <h1 class="ui header"><?php esc_html_e( 'Nothing Found', 'minimalflow' ); ?></h1>
         </header>
         <div class="ui message">
-            <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '' ); ?></p>
+            <p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'minimalflow' ); ?></p>
             <?php get_search_form(); ?>
         </div>
 
